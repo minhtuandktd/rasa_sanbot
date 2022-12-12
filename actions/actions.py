77e021c59b2d4ee4978a738dd964cb64dbaea3dc
Tuan Ngo Minh <minhtuandktd@gmail.com>
2022-12-12 16:37:51 +0700

    hoan thien kich ban sanbot

--- actions/actions.py
+++ actions/actions.py
@@ -26,30 +26,30 @@ class ActionHelloWorld(Action):
 
         return []
 
-import requests
+# import requests
 
-CRIC_API_URL = "https://api.cricapi.com/v1/"
-CRIC_API_KEY = "bade5f2e-fcc9-44b9-9659-2f0498e37dc0"
+# CRIC_API_URL = "https://api.cricapi.com/v1/"
+# CRIC_API_KEY = "bade5f2e-fcc9-44b9-9659-2f0498e37dc0"
 
-SHOW_RECENT_MATCHES = 5
+# SHOW_RECENT_MATCHES = 5
 
-class ActionGetRecentMatches(Action):
-	def name(self):
-		return 'action_get_recent_matches'
+# class ActionGetRecentMatches(Action):
+# 	def name(self):
+# 		return 'action_get_recent_matches'
 
-	def run(self, dispatcher, tracker, domain):
-		res = requests.get(CRIC_API_URL + "currentMatches" + "?apikey=" + CRIC_API_KEY + "&offset=0")
-		if res.status_code == 200:
-			matches_data = res.json()["data"]
-			matches_data.sort(key=lambda x: x["date"], reverse=True)
-			matches_data = [x for x in matches_data if "matchType" in x]
-			recent_matches = matches_data[:SHOW_RECENT_MATCHES]
-			dispatcher.utter_message(f'Showing the status of {len(recent_matches)} recent matches\n')
-			for index, match in enumerate(recent_matches):
-				msg = f'{index+1}. {match["matchType"].upper()} match between ' \
-					  f'{match["teams"][0]} and {match["teams"][1]} played on {match["date"]}\n' \
-					  f'Match Status: {match["status"]}\n'
-				dispatcher.utter_message(msg)
-		else:
-			dispatcher.utter_message(f'Unable to fetch the recent matches details. Please try with some other query!!')
-		return []
+# 	def run(self, dispatcher, tracker, domain):
+# 		res = requests.get(CRIC_API_URL + "currentMatches" + "?apikey=" + CRIC_API_KEY + "&offset=0")
+# 		if res.status_code == 200:
+# 			matches_data = res.json()["data"]
+# 			matches_data.sort(key=lambda x: x["date"], reverse=True)
+# 			matches_data = [x for x in matches_data if "matchType" in x]
+# 			recent_matches = matches_data[:SHOW_RECENT_MATCHES]
+# 			dispatcher.utter_message(f'Showing the status of {len(recent_matches)} recent matches\n')
+# 			for index, match in enumerate(recent_matches):
+# 				msg = f'{index+1}. {match["matchType"].upper()} match between ' \
+# 					  f'{match["teams"][0]} and {match["teams"][1]} played on {match["date"]}\n' \
+# 					  f'Match Status: {match["status"]}\n'
+# 				dispatcher.utter_message(msg)
+# 		else:
+# 			dispatcher.utter_message(f'Unable to fetch the recent matches details. Please try with some other query!!')
+# 		return []
